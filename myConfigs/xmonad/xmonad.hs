d3a10a75dd42aaeabe43e34d96c56654934f511a
Xiaoming94 <henyan94@gmail.com>
2014-03-26 00:21:14 +0100

    new backup

--- myConfigs/xmonad/xmonad.hs
+++ myConfigs/xmonad/xmonad.hs
@@ -5,6 +5,7 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
 import XMonad.Actions.CycleWS
@@ -26,11 +27,11 @@ import System.IO
 
 -- Define the name of all workspace
 
-myWorkspaces = ["main","web","chat","dev","misc","mail","cal"]
+myWorkspaces = ["main","web","chat","mail","dev","misc","music","cal"]
 
 -- Defining my Layout configurations. Quite lot of Layouts.
 
-myLayout = simpleFloat ||| ntiled ||| stiled ||| Mirror ntiled ||| Mirror stiled ||| 
+myLayout = ntiled ||| stiled ||| Mirror ntiled ||| Mirror stiled ||| 
   spiraled ||| Full ||| fullScreen
   where
     fullScreen = noBorders (fullscreenFull Full)
@@ -48,7 +49,7 @@ myLayout = simpleFloat ||| ntiled ||| stiled ||| Mirror ntiled ||| Mirror stiled
 
 myTerminal = "urxvt" -- the terminal I use
 myModMask = mod4Mask
-home = "spacefm"
+home = "pcmanfm"
 altMask = mod1Mask
 
 -- Configurations for Xmobar
@@ -66,37 +67,49 @@ toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 myManagedHooks = composeAll
   [ manageDocks
    ,isFullscreen --> doFullFloat
-   ,className=? "Chrome"  --> doShift "web"
-   ,className=? "calendar"  --> doShift "cal"
+   ,isDialog --> doCenterFloat
+   ,className =? "Chromium"  --> doShift "web"
+   ,className =? "Pidgin"   --> doShift "chat"
+   ,className =? "i3lock"   --> doFullFloat
+   ,className =? "Gimp"	   --> doFloat
+   ,className =? "Skype"    --> doShift "chat"
+   ,title =? "wyrd"  --> doShift "cal"
+   ,title =? "hubben"    --> doShift "chat"
+   ,manageHook defaultConfig
   ]
 
 -- Launching XMonad and some default overrides
 startup :: X ()
 startup = do
-          spawn "~/bin/xmonadautostart"
+          spawn "~/myscripts/bin/xmonadautostart"
           spawn "pulseaudio -k"
-          setWMName "LG3D"
-myConfig = defaultConfig { 
+
+myConfig = ewmh defaultConfig { 
     terminal    = myTerminal
   , modMask     = mod4Mask
   , startupHook = startup
   , manageHook  = myManagedHooks
   , borderWidth = 1
+  , handleEventHook = handleEventHook defaultConfig <+> XMonad.Hooks.EwmhDesktops.fullscreenEventHook
   , layoutHook  = lessBorders OnlyFloat $ avoidStruts $ myLayout
   , workspaces  = myWorkspaces
   , focusedBorderColor = "blue" 
   }`additionalKeys` -- My own keybindings.
     [((controlMask .|. altMask, xK_Left ), prevWS)
     ,((controlMask .|. altMask, xK_Right), nextWS)
-    ,((altMask     .|. shiftMask,   xK_w), spawn "chromium")
-    ,((myModMask   .|. shiftMask,   xK_d), raiseMaybe (runInTerm "-title calendar" "wyrd")(title =? "calendar"))
-    ,((myModMask   .|. controlMask, xK_m), spawn "evolution")
+    ,((altMask     .|. shiftMask,   xK_w), raiseMaybe (spawn "chromium")(className =? "Chromium"))
+    ,((myModMask   .|. shiftMask,   xK_d), raiseMaybe (runInTerm "EDITOR=vim" "wyrd")(title =? "wyrd"))
+    ,((myModMask   .|. controlMask, xK_m), spawn "geary")
     ,((controlMask,             xK_i),     raiseMaybe (runInTerm "-title hubben" "ssh hubben") (title =? "hubben"))
     ,((myModMask,               xK_p),     spawn "dmenu_run -fn 'inconsolata-10' -nb 'black' -nf 'red'")
     ,((myModMask,               xK_f),     spawn home)
     ,((0,                       xK_Print), spawn "scrot")
     ,((controlMask,             xK_Print), spawn "sleep 0.2; scrot -s")
     ,((myModMask,                   xK_x), spawn "slingshot-launcher")
+    -- For Sound Control
+--    ,((0,			0x1008ff11),		   spawn "amixer set Master 5%-")
+--    ,((0,			0x1008ff13),		   spawn "amixer set Master 5%+")
+--    ,((0,			0x1008ff12),		   spawn "amixer set Master toggle")
     ]
 
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig

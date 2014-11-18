a97b85786921e07d1688e9f20eaedc9314c94d98
Henry Yang <henyan94@gmail.com>
2014-11-19 00:58:27 +0100

    temporary stuff

--- myConfigs/xmonad/xmonad.hs
+++ myConfigs/xmonad/xmonad.hs
@@ -29,11 +29,11 @@ import System.IO
 
 -- Define the name of all workspace
 
-myWorkspaces = ["1:main","2:web","3:chat","4:dev","5:music","6:misc"]
+myWorkspaces = ["1:main","2:web","3:chat","4:dev","5:music","6:steam","7:misc"]
 
 -- Defining my Layout configurations. Quite lot of Layouts.
 
-myLayout = stiled ||| Mirror stiled ||| Mag.magnifier (Mirror stiled) |||
+myLayout = ntiled ||| stiled ||| Mirror ntiled ||| Mirror stiled ||| Mag.magnifier (Mirror stiled) |||
   spiraled ||| Full ||| fullScreen
   where
     fullScreen = noBorders (fullscreenFull Full)
@@ -64,6 +64,9 @@ myPP = xmobarPP {
                   ppCurrent = xmobarColor "green" "". wrap "[" "]"
                 , ppHidden = xmobarColor "blue" ""
                 , ppHiddenNoWindows = xmobarColor "white" ""
+                , ppLayout = xmobarColor "purple" ""
+                , ppUrgent = xmobarColor "red" ""
+                , ppTitle  = xmobarColor "#09cdda" ""
                 }
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
@@ -72,6 +75,7 @@ toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 myManagedHooks = composeAll
   [isFullscreen --> doFullFloat
    ,isDialog --> doCenterFloat
+   ,className =? "Google-chrome-stable"  --> doShift "2:web"
    ,className =? "Chromium"  --> doShift "2:web"
    ,className =? "Pidgin"   --> doShift "3:chat"
    ,className =? "i3lock"   --> doFullFloat
@@ -79,6 +83,8 @@ myManagedHooks = composeAll
    ,className =? "Skype"    --> doShift "3:chat"
    ,className =? "Spotify"  --> doShift "5:music"
    ,className =? "Florence" --> doIgnore
+   ,className =? "Steam"    --> doShift "6:steam"
+   ,title =? "Slingshot"    --> doFullFloat
    ,title =? "hubben"    --> doShift "3:chat"
   ]
 
@@ -87,21 +93,23 @@ startup :: X ()
 startup = do
           -- spawn "pulseaudio -k"
           setWMName "LG3D"
+          spawn "~/bin/myscripts/xmonadautostart"
 
 myConfig = ewmh defaultConfig { 
     terminal    = myTerminal
   , modMask     = mod4Mask
   , startupHook = setWMName "LG3D" <+> startup
   , manageHook  = manageDocks <+> myManagedHooks <+> manageHook defaultConfig
-  , borderWidth = 1
+  , borderWidth = 2
   , handleEventHook = handleEventHook defaultConfig <+> XMonad.Hooks.EwmhDesktops.fullscreenEventHook
   , layoutHook  = lessBorders OnlyFloat $ avoidStruts $ myLayout
   , workspaces  = myWorkspaces
-  , focusedBorderColor = "#0000ff"
+  , focusedBorderColor = "darkblue"
+  , normalBorderColor = "#313131"
   }`additionalKeys` -- My own keybindings.
     [((controlMask .|. altMask, xK_Left ), prevWS)
     ,((controlMask .|. altMask, xK_Right), nextWS)
-    ,((altMask     .|. shiftMask,   xK_w), raiseMaybe (spawn web )(className =? "Chromium"))
+    ,((altMask     .|. shiftMask,   xK_w), raiseMaybe (spawn web )(className =? "Google-chrome-stable"))
     ,((myModMask   .|. shiftMask,   xK_d), raiseMaybe (runInTerm "EDITOR=vim" "wyrd")(title =? "wyrd"))
     ,((myModMask   .|. controlMask, xK_m), spawn "geary")
     ,((controlMask,             xK_i),     raiseMaybe (runInTerm "-title hubben" "ssh hubben") (title =? "hubben"))
@@ -112,7 +120,9 @@ myConfig = ewmh defaultConfig {
     ,((myModMask,                   xK_x), spawn "slingshot-launcher")
     ,((myModMask   .|. controlMask, xK_h), sendMessage Mag.MagnifyMore)
     ,((myModMask   .|. controlMask, xK_l), sendMessage Mag.MagnifyLess)
-    
+    ,((myModMask   .|. controlMask, xK_t), spawn "togglecom compton")
+    ,((myModMask, xK_s), spawn "togglecom conky")
+
     -- For Sound Control
 --    ,((0,			0x1008ff11),		   spawn "amixer set Master 5%-")
 --    ,((0,			0x1008ff13),		   spawn "amixer set Master 5%+")
